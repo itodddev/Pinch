7ed7e67a000677e80ad2a7b1ece2d1d4606fc571
Todd James <todd@itodd.co>
2022-07-01 14:16:24 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Todd James on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
